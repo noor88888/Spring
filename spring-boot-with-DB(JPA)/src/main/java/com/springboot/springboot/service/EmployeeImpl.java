66235c2ad45447eae54d481d2e273068838129a5
Noor Ahamed S <Noor Ahamed S@LAPTOP-2MFVMH14>
2022-09-18 17:02:33 +0530

    complete working

--- spring-boot-with-DB(JPA)/src/main/java/com/springboot/springboot/service/EmployeeImpl.java
+++ spring-boot-with-DB(JPA)/src/main/java/com/springboot/springboot/service/EmployeeImpl.java
@@ -24,18 +24,18 @@ public class EmployeeImpl implements EmployeeService
 	@Override
 	public Employee getEmployeeById(int eid) 
 	{
-		return er.findById(eid).orElseThrow(()->new EmployeeNotFoundException("Employee "+ eid + " not found try seraching with other names"));
+		return er.findById(eid).orElseThrow(()->new EmployeeNotFoundException("Employee with id:"+ eid + " not found try seraching with other id"));
 	}
 	@Override
 	public void deleteEmployeeById(int eid)
 	{	
-		er.findById(eid).orElseThrow(()->new EmployeeNotFoundException("Employee with id:"+ eid + " not found enter correct id to delete"));
+		er.findById(eid).orElseThrow(()->new EmployeeNotFoundException("Employee with id:"+ eid + " not found ,enter correct id to delete"));
 		er.deleteById(eid);
 	}
 	@Override
 	public Employee updateEmployee(Employee employee ,int eid)
 	{
-		Employee existingEmp=er.findById(eid).orElseThrow(()->new EmployeeNotFoundException("Employee with id:"+ eid + " not found enter correct id to update"));
+		Employee existingEmp=er.findById(eid).orElseThrow(()->new EmployeeNotFoundException("Employee with id:"+ eid + " not found ,enter correct id to update"));
 		existingEmp.setName(employee.getName());
 		existingEmp.setDepartment(employee.getDepartment());
 		existingEmp.setLocation(employee.getLocation());
